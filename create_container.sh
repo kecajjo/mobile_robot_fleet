641fac6f96e38be8525620f58dfe6bc24e14b2bc
Bartłomiej <61778420+b-stadnik@users.noreply.github.com>
2022-10-14 12:03:53 +0200

    Update create container script (#2)
    * Update create container script
    
    * Separate container name from image name
    
    Co-authored-by: bartek-sta <bartlomiej.stadnik@unitem.com>
    Co-authored-by: bstadnik <bartlomiej.stadnik@unitem.pl>

--- create_container.sh
+++ create_container.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 scriptDir=$(dirname $0 | xargs -i readlink -f {})
-container_name=mobile_robot_fleet
+image_name="mobile_robot_fleet"
+container_name=$image_name
 version="0.1"
-does_exist=$(docker image ls $container_name:$version | grep -ci1 $container_name)
+
+function print_banner() {
+    echo ">> Image version to be used: $version"
+    echo ">> Mounting host directory \"$scriptDir\" under \"/catkin_ws\" share.
+
+    To enter the container type:
+
+      $ docker exec -it $container_name /bin/bash
+
+    To build the workspace inside container type:
+
+      $ catkin build
+      "
+}
+
+for option in "$@"; do
+    case "$option" in
+        -n|--name)
+            container_name="$2"
+            shift 2
+            ;;
+    esac
+done
+
+does_exist=$(docker image ls $image_name:$version | grep -ci1 $image_name)
 if [ $does_exist == "0" ] ; then
-	docker build -t $container_name:$version .
+	docker build -t $image_name:$version .
 fi
-docker run --rm \
+
+print_banner
+docker run \
+    --name "$container_name" \
     --privileged \
     --env DISPLAY \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v "$scriptDir/src/:/home/developer/catkin_ws/src" \
-    -it $container_name:$version /bin/bash
+    -itd $image_name:$version /bin/bash
