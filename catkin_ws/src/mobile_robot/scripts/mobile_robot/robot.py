f153a57135d9fddae818d6f64e2d34dce51e88af
bartek-sta <bartlomiej.stadnik77@gmail.com>
2022-12-03 23:54:06 +0100

    Switch to common topics/services for all robots

---
+++ catkin_ws/src/mobile_robot/scripts/mobile_robot/robot.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import rospy
+import queue
+from common_msgs.msg import RobotState, RobotStatus, WarehouseLocation
+
+WAIT_TIME_PER_PART = 2.0
+WAIT_TIME_MOVE = 5.0
+
+class Robot(object):
+    def __init__(self, id, capacity):
+        self._id = id
+        self._capacity = capacity
+
+        self._carried_parts = queue.Queue(maxsize=self._capacity)
+        self._state = RobotState.READY
+        self._location = WarehouseLocation.RESTING_AREA
+        
+        self._pub_robot_status = rospy.Publisher('/robot/status', RobotStatus, queue_size=10)
+        self._publish_robot_status()
+        
+        rospy.loginfo("Robot_{} has started succesfuly!".format(self._id))
+        
+    def _move_to(self, location):
+        self._location = location
+        self._state = RobotState.MOVING
+        self._publish_robot_status()
+
+        rospy.Timer(rospy.Duration(WAIT_TIME_MOVE), self._timed_task_finished, oneshot=True)
+
+    def _get_parts(self, parts_qty):
+        self._state = RobotState.WORKING
+        self._publish_robot_status()
+        
+        for _ in range(parts_qty):
+            self._carried_parts.put(self._location)  # put part of ID inherited from current location
+
+        rospy.Timer(rospy.Duration(WAIT_TIME_PER_PART * parts_qty), self._timed_task_finished, oneshot=True)
+
+    def _drop_parts(self):
+        self._state = RobotState.WORKING
+        self._publish_robot_status()
+
+        rospy.Timer(rospy.Duration(WAIT_TIME_PER_PART * len(self._carried_parts.queue)),
+                    self._timed_task_finished, oneshot=True)
+        self._carried_parts.queue.clear()
+
+    def _publish_robot_status(self):
+        msg = RobotStatus()
+        msg.state = RobotState(self._state)
+        msg.location = WarehouseLocation(self._location)
+        msg.carried_parts = list(self._carried_parts.queue)
+        msg.id = self._id
+        self._pub_robot_status.publish(msg)
+
+    def _timed_task_finished(self, event):
+        self._state = RobotState.READY
+        self._publish_robot_status()
---
+++ catkin_ws/src/mobile_robot/scripts/robot_node.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import rospy
+from common_msgs.srv import CommandRobot, CommandRobotResponse
+from common_msgs.msg import RobotState
+
+from mobile_robot.robot import Robot
+
+class RobotsHandler(object):
+    def __init__(self):
+        robots_qty =  rospy.get_param('~robots_qty')
+        capacity = rospy.get_param('~robot_capacity')
+        self._robots = {}
+        for id in range(robots_qty):
+            self._robots[id] = Robot(id, capacity)
+        
+        self._service_move_to = rospy.Service('robot/move_to', CommandRobot, self._response_move_to)
+        self._service_get_parts = rospy.Service('/robot/get_parts', CommandRobot, self._response_get_parts)
+        self._service_drop_parts = rospy.Service('/robot/drop_parts', CommandRobot, self._response_drop_parts)
+
+    def _response_move_to(self, req):
+        robot = self._robots.get(req.robot_id)
+        if robot._state is not RobotState.READY:
+            return CommandRobotResponse(False)
+        robot._move_to(req.value)
+        return CommandRobotResponse(True)
+
+    def _response_get_parts(self, req):
+        robot = self._robots.get(req.robot_id)
+        if (req.value > robot._capacity - len(robot._carried_parts.queue)) or robot._state is not RobotState.READY:
+            return CommandRobotResponse(False)
+        robot._get_parts(req.value)
+        return CommandRobotResponse(True)
+
+    def _response_drop_parts(self, req):
+        robot = self._robots.get(req.robot_id)
+        if robot._carried_parts.empty() or robot._state is not RobotState.READY:
+            return CommandRobotResponse(False)
+        robot._drop_parts()
+        return CommandRobotResponse(True)
+
+
+if __name__ == '__main__':
+    rospy.init_node('mobile_robot')
+    rospy.loginfo("Starting robots...")
+
+    robot = RobotsHandler()
+    rospy.spin()
